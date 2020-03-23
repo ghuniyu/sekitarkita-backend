d58e70b55267c8bb83885167a68cfb5bd39687ab
Willy Robertus <willy@ritase.com>
2020-03-23 23:15:32 +0700

    Add route to direct download the APK

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/landing');
 });
+
+Route::get('/download', function ($id) {
+    return redirect('/public/sekitarkita.apk');
+});
