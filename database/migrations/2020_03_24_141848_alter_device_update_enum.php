82437885deef6255336e606094a706edd0e935cd
iamnubs <ghuniyu.fattah.rozaq@gmail.com>
2020-03-24 21:59:28 +0700

    + update add speed column and change health enum

--- database/migrations/2020_03_24_141848_alter_device_update_enum.php
+++ database/migrations/2020_03_24_141848_alter_device_update_enum.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class AlterDeviceAddSpeedAndEnum extends Migration
+class AlterDeviceUpdateEnum extends Migration
 {
     /**
      * Run the migrations.
@@ -16,7 +16,6 @@ class AlterDeviceAddSpeedAndEnum extends Migration
     {
         Schema::table('devices', function (Blueprint $table) {
             DB::statement("ALTER TABLE devices CHANGE COLUMN health_condition health_condition ENUM('healthy', 'pdp', 'odp', 'confirmed') NOT NULL DEFAULT 'healthy'");
-            $table->double('speed')->nullable();
         });
     }
 
@@ -29,7 +28,6 @@ class AlterDeviceAddSpeedAndEnum extends Migration
     {
         Schema::table('devices', function (Blueprint $table) {
             DB::statement("ALTER TABLE devices CHANGE COLUMN health_condition health_condition ENUM('healthy', 'pdp', 'odp') NOT NULL DEFAULT 'healthy'");
-            $table->dropColumn('speed');
         });
     }
 }
