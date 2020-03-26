ba1ec2e2a0eceb1bde8b3e9af812a1c879d3e765
Alif Jafar <alifjafar@gmail.com>
2020-03-26 22:11:22 +0700

    Refactor view

--- resources/views/mapping/base.blade.php
+++ resources/views/mapping/base.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-@section('title', 'Mapping Member -')
 @section('content')
     <div class="container-fluid">
         <div class="row">
@@ -18,16 +17,11 @@
             <div class="col-md-12 m-lg-5 text-center">
                 <h5 class="mb-lg-2">Legend</h5>
                 <br>
-                <img src="/images/icons/smartphone_healthy.svg" style="max-height: 20px" alt=""><span class="mr-lg-4">Sehat</span>
-                <img src="/images/icons/smartphone_odp.svg" style="max-height: 20px" alt=""><span class="mr-lg-4">ODP</span>
-                <img src="/images/icons/smartphone_pdp.svg" style="max-height: 20px" alt=""><span class="mr-lg-4">PDP</span>
-                <img src="/images/icons/smartphone_confirmed.svg" style="max-height: 20px" alt=""><span class="mr-lg-4">Positif</span>
+                <img src="/images/icons/smartphone_healthy.svg" style="height: 20px" alt=""><span class="mr-lg-4">Sehat</span>
+                <img src="/images/icons/smartphone_odp.svg" style="height: 20px" alt=""><span class="mr-lg-4">ODP</span>
+                <img src="/images/icons/smartphone_pdp.svg" style="height: 20px" alt=""><span class="mr-lg-4">PDP</span>
+                <img src="/images/icons/smartphone_confirmed.svg" style="height: 20px" alt=""><span class="mr-lg-4">Positif</span>
             </div>
         </div>
     </div>
 @endsection
-@push('js')
-    <script type="text/javascript">
-        visualNetwork('/api/member-interaction?only=odp', null, 1);
-    </script>
-@endpush
