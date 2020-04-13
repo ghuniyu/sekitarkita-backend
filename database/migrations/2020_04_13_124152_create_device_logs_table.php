aef53de89c657dabebe00f7988dc7ee5c197b828
iamnubs <ghuniyu.fattah.rozaq@gmail.com>
2020-04-13 20:05:47 +0700

    Device Logging and Request

---
+++ database/migrations/2020_04_13_124152_create_device_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeviceLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('device_logs', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('device_id');
+            $table->foreign('device_id')->references('id')->on('devices');
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('device_logs');
+    }
+}
