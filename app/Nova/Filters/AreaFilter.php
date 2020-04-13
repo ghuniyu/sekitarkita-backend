5473de8ae1746dafc9a6d8295467e8a9cb3f0ba6
iamnubs <ghuniyu.fattah.rozaq@gmail.com>
2020-04-14 02:56:28 +0700

    Add Filter temporary using multi select

---
+++ app/Nova/Filters/AreaFilter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Nova\Filters;
+
+use App\Models\DeviceLog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Laravel\Nova\Filters\Filter;
+
+class AreaFilter extends Filter
+{
+    /**
+     * The filter's component.
+     *
+     * @var string
+     */
+    public $component = 'select-filter';
+
+    /**
+     * Apply the filter to the given query.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param mixed $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Request $request, $query, $value)
+    {
+        $ids = $query->where('area', $value)->get(['id', 'device_id'])->unique('device_id')->pluck('id');
+        return $query->whereIn('id', $ids);
+    }
+
+    /**
+     * Get the filter's available options.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function options(Request $request)
+    {
+        return DeviceLog::whereNotNull('area')->get('area')->pluck('area')->unique()->map(function ($v) {
+            return [
+                'id' => $v,
+                'name' => $v,
+                'value' => $v,
+            ];
+        });
+    }
+}
