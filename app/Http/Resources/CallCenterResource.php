e8bf91da6616e4fdc6064a1fc1645c2b748771f4
Alif Jafar <alifjafar@gmail.com>
2020-05-30 17:29:39 +0700

    fix: CallCenterResource

--- app/Http/Resources/CallCenterResource.php
+++ app/Http/Resources/CallCenterResource.php
@@ -18,18 +18,18 @@ class CallCenterResource extends JsonResource
     public function toArray($request)
     {
         if ($this['area'] instanceof Kecamatan) {
-            $area = sprintf("Kecamatan %s, Kabupaten %s, Provinsi %s", Str::title(data_get($this['area'], 'nama')), Str::title(data_get($this['area'], 'kabupaten.nama')), Str::title(data_get($this['area'], 'kabupaten.provinsi.nama')));
+            $area = sprintf("Kecamatan %s, Kabupaten %s, Provinsi %s", Str::title(data_get($this['area'], 'name')), Str::title(data_get($this['area'], 'kabupaten.name')), Str::title(data_get($this['area'], 'kabupaten.provinsi.name')));
         } elseif ($this['area'] instanceof Kabupaten) {
-            $area = sprintf("Kabupaten %s, Provinsi %s", Str::title(data_get($this['area'], 'nama')), Str::title(data_get($this['area'], 'provinsi.nama')));
+            $area = sprintf("Kabupaten %s, Provinsi %s", Str::title(data_get($this['area'], 'name')), Str::title(data_get($this['area'], 'provinsi.name')));
         } else {
-            $area = sprintf("Provinsi %s", Str::title(data_get($this['area'], 'nama')));
+            $area = sprintf("Provinsi %s", Str::title(data_get($this['area'], 'name')));
         }
 
         return [
             'phone' => $this['phone'],
             'mobile' => $this['mobile'],
             'website' => $this['website'],
-            'area' => $this['area']['nama'],
+            'area' => $this['area']['name'],
             'area_detail' => $area,
         ];
     }
