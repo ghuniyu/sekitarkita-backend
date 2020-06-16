0efed603f5d740f4543ece465d52f61806de110f
Alif Jafar <alifjafar@gmail.com>
2020-06-16 12:49:18 +0700

    cache area

--- app/Http/Controllers/Api/AreaController.php
+++ app/Http/Controllers/Api/AreaController.php
@@ -32,35 +32,43 @@ class AreaController extends Controller
 
     function getGorontalo()
     {
-        $data = Provinsi::with('kabupatens.kecamatans.kelurahans')->find(75);
-        return [
-            'data' => $data['kabupatens']->map(function ($kab) {
-                return $kab['kecamatans']->map(function ($kec) use ($kab) {
-                    return $kec['kelurahans']->map(function ($des) use ($kab, $kec) {
-                        return [
-                            'id' => $des['id'],
-                            'name' => sprintf('%s, %s, %s', $kab['name'], $kec['name'], $des['name'])
-                        ];
-                    });
-                });
-            })->flatten(2),
-            'success' => true
-        ];
+       $result = cache()->remember('get-area-gorontalo',  now()->addHours(12), function () {
+           $data = Provinsi::with('kabupatens.kecamatans.kelurahans')->find(75);
+           return [
+               'data' => $data['kabupatens']->map(function ($kab) {
+                   return $kab['kecamatans']->map(function ($kec) use ($kab) {
+                       return $kec['kelurahans']->map(function ($des) use ($kab, $kec) {
+                           return [
+                               'id' => $des['id'],
+                               'name' => sprintf('%s, %s, %s', $kab['name'], $kec['name'], $des['name'])
+                           ];
+                       });
+                   });
+               })->flatten(2),
+               'success' => true
+           ];
+       });
+
+       return response()->json($result);
     }
 
     function getOriginCities()
     {
-        $data = Provinsi::with('kabupatens')->get();
-        return [
-            'data' => $data->map(function ($prov) {
-                return $prov['kabupatens']->map(function ($kab) use ($prov) {
-                    return [
-                        'id' => $kab['id'],
-                        'name' => sprintf('%s, %s', $prov['name'], $kab['name'])
-                    ];
-                });
-            })->flatten(1),
-            'success' => true
-        ];
+        $result = cache()->remember('get-all-area', now()->addHours(12), function () {
+            $data = Provinsi::with('kabupatens')->get();
+            return [
+                'data' => $data->map(function ($prov) {
+                    return $prov['kabupatens']->map(function ($kab) use ($prov) {
+                        return [
+                            'id' => $kab['id'],
+                            'name' => sprintf('%s, %s', $prov['name'], $kab['name'])
+                        ];
+                    });
+                })->flatten(1),
+                'success' => true
+            ];
+        });
+
+        return response()->json($result);
     }
 }
